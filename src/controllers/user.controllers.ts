94308bcd4bc0c94788fd941f28e9362eadbe388f
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-12-26 19:50:46 -0300

    tsconfig FIX

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,19 +1,22 @@
-import { PrismaClient } from '@prisma/client'
-import { Request, Response, NextFunction } from 'express';
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+const prisma = new PrismaClient();
 
-export const getUsers = async (req: Request, res: Response) => {
+export const getUsers = async (req: Request, res: Response): Promise<void> => {
     try {
-        const users = await prisma.user.findFirst()
+        const users = await prisma.user.findFirst();
         res.json({
             message: 'Successful response',
-            data: users
-        })
+            data: users,
+        });
     } catch (error: any) {
-        console.log(error.message)
-        return res.status(500).json({ error: error.message })
+        console.log(error.message);
+        res.status(500).json({ error: error.message });
     }
-}
+};
+
+
+
 /*
 export const getUser = async (req, res) => {
     try {
