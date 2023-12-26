eaeec93264df224e0a1383f7a8e050a10ee1c832
Lucho <luis.herrero.facultad@gmail.com>
2023-12-26 18:53:21 -0300

    commit

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client')
+import { PrismaClient } from '@prisma/client'
 import { Request, Response, NextFunction } from 'express';
 const prisma = new PrismaClient()
 
